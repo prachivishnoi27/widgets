7c4c3c4b006dac9ea68dbc0877d6bd90452f9d1c
prachivishnoi <prachi27vishnoi@gmail.com>
2021-01-14 16:45:11 +0530

    dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,33 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
 
-    const renderedOptions = options.map( (option) => {
-        if( option.value === selected.value){
-            return null
-        }
-        return (
-            <div key={option.value} className="item" onClick={() => onSelectedChange(option)} >
-                {option.label}
-            </div>
-        )
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      console.log(ref);
+        // if(ref.current.contains(event.target)){
+        //     console.log('contains');
+        //     return;
+        // }
+        console.log("Event Target ", event.target);
+        console.log('BODY CLICKED');
+        setOpen(false)
     });
+  }, []);
 
+
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
     return (
-        <div className="ui form">
-            <div className="field">
-                <label className="label">Select a Color</label>
-                <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active': ''}`}>
-                    <i className="dropdown icon"></i>
-                    <div className="text">{selected.label}</div>
-                    <div className={`menu ${open? 'visible transition': ''}`}>
-                        {renderedOptions}
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => {
+            console.log('ITEM CLICKED');
+          onSelectedChange(option);
+        }}
+      >
+        {option.label}
+      </div>
     );
-}
+  });
+
+  return (
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div 
+          onClick={() => {
+            setOpen(!open);
+            console.log("DROPDOWN CLICKED");
+          }}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dropdown;
